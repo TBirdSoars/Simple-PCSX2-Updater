93546ad79d5d287d83b2232682856cefbb57b20e
TBirdSoars <31261091+TBirdSoars@users.noreply.github.com>
2021-08-17 19:47:31 -0400

    c l e an u op

--- Program.cs
+++ Program.cs
@@ -64,37 +64,45 @@ namespace Simple_PCSX2_Updater
             // Proceed?
             if (response == ConsoleKey.Y)
             {
-                // Get build list and download URL
+                // Get build list
                 Console.WriteLine("Getting build list... ");
                 DataTable buildTable = await GetBuildTable();
-                string build_Path_and_Query = buildTable.Rows[0]["build"].ToString().Replace("amp;", "");
-                Uri downloadURL = new Uri(baseURL + build_Path_and_Query);
+
+                if(buildTable.Rows.Count > 0)
+                {
+                    // Get download URL
+                    string build_Path_and_Query = buildTable.Rows[0]["build"].ToString().Replace("amp;", "");
+                    Uri downloadURL = new Uri(baseURL + build_Path_and_Query);
 
 
-                // Get name of extract folder
-                string folderName = "pcsx2-" + HttpUtility.ParseQueryString(downloadURL.Query).Get("rev");
-                folderName += "-" + HttpUtility.ParseQueryString(downloadURL.Query).Get("platform");
-                string extractFolder = Path.Combine(currentDir, folderName);
+                    // Get name of extract folder
+                    string folderName = "pcsx2-" + HttpUtility.ParseQueryString(downloadURL.Query).Get("rev");
+                    folderName += "-" + HttpUtility.ParseQueryString(downloadURL.Query).Get("platform");
+                    string extractFolder = Path.Combine(currentDir, folderName);
 
 
-                // Get download from URL, from finalTable
-                Console.WriteLine($"Downloading version {folderName}... ");
-                await DownloadArchive(downloadURL, zipFullDir);
+                    // Get download from URL, from finalTable
+                    Console.WriteLine($"Downloading version {folderName}... ");
+                    await DownloadArchive(downloadURL, zipFullDir);
 
 
-                // Extract 7zip archive
-                Console.WriteLine("Extracting PCSX2... ");
-                ExtractArchive(zipFullDir);
+                    // Extract 7zip archive
+                    Console.WriteLine("Extracting PCSX2... ");
+                    ExtractArchive(zipFullDir);
 
 
-                // Move files into pcsx2.exe directory
-                Console.WriteLine("Moving files...");
-                //MoveAll(extractFolder, currentDir);
-                MoveAll(extractFolder, currentDir);
+                    // Move files into pcsx2.exe directory
+                    Console.WriteLine("Moving files...");
+                    MoveAll(extractFolder, currentDir);
 
 
-                // Done!
-                Console.WriteLine("Done!");
+                    // Done!
+                    Console.WriteLine("Done!");
+                }
+                else
+                {
+                    Console.WriteLine("Failed to get list of builds.");
+                }
             }
 
             // End execution
@@ -103,7 +111,6 @@ namespace Simple_PCSX2_Updater
             Console.ReadKey();
         }
 
-        // CLEAN THIS SHIT
         private static async Task<DataTable> GetBuildTable()
         {
             DataTable output = new DataTable();
@@ -123,58 +130,66 @@ namespace Simple_PCSX2_Updater
             // Get table with all recent releases
             HtmlNode tableNode = htmlDoc.DocumentNode.SelectSingleNode("//table[@class='listing']");
             // OK - get items of table, skip first item of table, only get items that contain more than one element, get the table as a list of lists of htmlnodes?!
-            List<List<HtmlNode>> tableListList = tableNode.Descendants("tr").Skip(1).Where(tr => tr.Elements("td").Count() > 1).Select(tr => tr.Elements("td").ToList()).ToList();
-
-            // Convert list of lists to datatable
-            DataTable releaseTable = new DataTable();
-            releaseTable.Columns.Add("commit", typeof(string));
-            releaseTable.Columns.Add("username", typeof(string));
-            releaseTable.Columns.Add("date", typeof(string));
-            releaseTable.Columns.Add("build", typeof(string));
-            releaseTable.Columns.Add("change", typeof(string));
-            foreach (List<HtmlNode> nodeList in tableListList)
+            // First make sure stuff exists
+            if(tableNode != null)
             {
-                DataRow row = releaseTable.NewRow();
+                List<List<HtmlNode>> tableListList = tableNode.Descendants("tr").Skip(1).Where(tr => tr.Elements("td").Count() > 1).Select(tr => tr.Elements("td").ToList()).ToList();
 
-                for (int i = 0; i < nodeList.Count; i++)
+                // Convert list of lists to datatable
+                DataTable releaseTable = new DataTable();
+                releaseTable.Columns.Add("commit", typeof(string));
+                releaseTable.Columns.Add("username", typeof(string));
+                releaseTable.Columns.Add("date", typeof(string));
+                releaseTable.Columns.Add("build", typeof(string));
+                releaseTable.Columns.Add("change", typeof(string));
+                foreach (List<HtmlNode> nodeList in tableListList)
                 {
-                    // The attributes are on the child nodes... I swear to fucking god I will never touch HTML again
-                    // Check if there is an href attribute, if not just grab innerText or nothing
-                    if (nodeList[i].HasChildNodes)
+                    DataRow row = releaseTable.NewRow();
+
+                    for (int i = 0; i < nodeList.Count; i++)
                     {
-                        // Get attribute from childnode
-                        if (nodeList[i].FirstChild.Attributes["href"] != null)
+                        // The attributes are on the child nodes... I swear to fucking god I will never touch HTML again
+                        if (nodeList[i].HasChildNodes)
                         {
-                            row[i] = nodeList[i].FirstChild.Attributes["href"].Value;
+                            // Check if there is an href attribute, if not just grab innerText or nothing
+                            if (nodeList[i].FirstChild.Attributes["href"] != null)
+                            {
+                                row[i] = nodeList[i].FirstChild.Attributes["href"].Value;
+                            }
+                            else
+                            {
+                                row[i] = nodeList[i].FirstChild.InnerText;
+                            }
                         }
                         else
                         {
-                            row[i] = nodeList[i].FirstChild.InnerText;
+                            // No child nodes, just get outerHTML
+                            row[i] = nodeList[i].OuterHtml;
                         }
                     }
-                    else
-                    {
-                        // No child nodes, just get outerHTML
-                        row[i] = nodeList[i].OuterHtml;
-                    }
+
+                    releaseTable.Rows.Add(row);
                 }
 
-                releaseTable.Rows.Add(row);
+                // Remove "No build" entries -  we want only downloads
+                releaseTable = releaseTable.Select("build <> 'No build'").CopyToDataTable();
+
+                // Convert the date column to datetime, then sort to find newest
+                DataTable finalTable = releaseTable.Clone();
+                finalTable.Columns["date"].DataType = typeof(DateTime);
+                foreach (DataRow row in releaseTable.Rows)
+                {
+                    finalTable.ImportRow(row);
+                }
+                finalTable.DefaultView.Sort = "date DESC";
+                output = finalTable.DefaultView.ToTable();
             }
-
-            // Remove "No build" entries
-            DataTable buildTable = releaseTable.Select("build <> 'No build'").CopyToDataTable();
-
-            // Convert the date column to datetime, then sort to find newest
-            DataTable finalTable = buildTable.Clone();
-            finalTable.Columns["date"].DataType = typeof(DateTime);
-            foreach (DataRow row in buildTable.Rows)
+            else
             {
-                finalTable.ImportRow(row);
+                Console.WriteLine($"{baseURL} returned null");
             }
-            finalTable.DefaultView.Sort = "date DESC";
 
-            return finalTable = finalTable.DefaultView.ToTable();
+            return output;
         }
 
         private static async Task DownloadArchive(Uri uri, string dest)
